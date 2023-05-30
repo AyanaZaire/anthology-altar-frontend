be4d31e793cebe9610558bdf530f39bdecef0a2f
AyanaZaire <ayanazaire@gmail.com>
2023-05-30 15:12:27 -0400

    css and seed data

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# loophole-altar
+# Loophole of Retreat Altar
+
+
+## How To Get Started 
+1. Fork and clone this project.
+2. Open your terminal and run `$ json-server --watch db.json` in the project directory to start the server.
+3. Then open the `index.html` file in your browser.
--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("hello!");
+document.addEventListener("DOMContentLoaded", () => {
+    loadRetreaters()
+})
+
+function loadRetreaters() {
+    fetch("http://localhost:3000/retreaters")
+    .then(response => response.json())
+    .then(retreaters => {
+        displayRetreatersNav(retreaters)
+        displayRetreatersMain(retreaters)
+    })
+}
+
+function displayRetreatersNav(retreaters) {
+    let navUl = document.querySelector("#nav-ul")
+    retreaters.forEach(retreater => {
+        let li =
+        `<li>
+            <a class="nav-link" href="#${retreater.name}">${retreater.name}</a>
+        </li>`
+        navUl.innerHTML += li
+    })
+}
+
+function displayRetreatersMain(retreaters) {
+    let mainDoc = document.querySelector("#main-doc")
+    retreaters.forEach(retreater => {
+        let mainSection = 
+        `<section class="main-section" id="${retreater.name}">
+            <header>${retreater.name}</header>
+            <iframe width="560" height="315" src="${retreater.youtubeLink}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <article>
+            ${retreater.essayTitle}
+            <p>${retreater.essayBody}</p>
+            </article>
+        </section>`
+        mainDoc.innerHTML += mainSection
+    })
+}
