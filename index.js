5ae0924570f3e6c6bc951810fd23cc972712590b
AyanaZaire <ayanazaire@gmail.com>
2023-06-21 12:37:48 -0400

    updated db.json

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
     loadPoems()
-    createPoem()
+    let poemForm = document.getElementById('poem-form') 
+    poemForm.addEventListener("submit", (event) => {
+        poemFormHandler(event)
+    })
 })
 
 function loadPoems() {
     fetch("http://localhost:3000/poems")
     .then(response => response.json())
     .then(poems => {
-        displayPoemsNav(poems)
-        displayPoemsMain(poems)
+        poems.forEach(poem => {
+            displayPoem(poem)
+        })
     })
 }
 
-function displayPoemsNav(poems) {
+function displayPoem(poem) {
+    // display in nav
     let navUl = document.querySelector("#nav-ul")
-    poems.forEach(poem => {
+    // poems.forEach(poem => {
         let li =
         `<li>
             <a class="nav-link" href="#${poem.title}">${poem.title}</a>
             <p>${poem.author}<span>${poem.year}</span></p>
         </li>`
         navUl.innerHTML += li
-    })
-}
+    // })
 
-function displayPoemsMain(poems) {
+    // display in main doc
     let mainDoc = document.querySelector("#main-doc")
-    poems.forEach(poem => {
+    // poems.forEach(poem => {
         if (youtubeVideo != "") {
             var youtubeVideo = `<iframe width="560" height="315" src="${poem.video}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
         }
@@ -39,12 +43,60 @@ function displayPoemsMain(poems) {
             <p class="author">${poem.author}</p>
             <p class="year">${poem.year}</p>
             <p>${poem.poem}</p>
+            <div class="flex-container">
+                <div class="flex-child">
+                    <p class="citation">Citation</p>
+                </div>
+                <div class="flex-child">
+                    <p class="citation-text">${poem.author}.<i>${poem.title}</i>.${poem.publisher}, ${poem.year}</p>
+                </div>
+            </div>
             </article>
         </section>`
         mainDoc.innerHTML += mainSection
-    })
+    // })
 }
 
-function createPoem() {
-    
+// called inside DOMContentLoaded event listener 
+function poemFormHandler(event) {
+    event.preventDefault ? event.preventDefault() : (event.returnValue = false);
+    let inputTitle = document.getElementById('title').value
+    let inputAuthor = document.getElementById('author').value
+    let inputPoem = document.getElementById('poem').value
+    let inputSource = document.getElementById('source').value
+    let inputBook = document.getElementById('book').value
+    let inputPublisher = document.getElementById('publisher').value
+    let inputYear = document.getElementById('year').value
+    let inputImage = document.getElementById('image').value
+    let inputVideo = document.getElementById('video').value
+    console.log(inputAuthor)
+    createPoem(inputTitle, inputAuthor, inputPoem, inputSource, inputBook, inputPublisher, inputYear, inputImage, inputVideo)
+    return false;
+}
+
+function createPoem(inputTitle, inputAuthor, inputPoem, inputSource, inputBook, inputPublisher, inputYear, inputImage, inputVideo) {
+    fetch('http://localhost:3000/poems', {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({
+        title: inputTitle, 
+        author: inputAuthor, 
+        poem: inputPoem, 
+        source: inputSource, 
+        book: inputBook, 
+        publisher: inputPublisher, 
+        year: inputYear, 
+        image: inputImage, 
+        video: inputVideo
+    })
+  })
+  .then(response => response.json())
+  .then(json => {
+      if(!json.error) {
+        console.log(json)
+        displayPoem(json)
+      } else {
+        alert(json.error.message)
+      }
+  })
 }
